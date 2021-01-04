a89d5903939442bbd8ad4b467f8cdb846061a7a7
Alex Baratti <alexsmbaratti@gmail.com>
2021-01-03 17:03:20 -0800

    Renders software projects dynamically

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Alex Baratti'});
+    var json = JSON.parse(fs.readFileSync('./data.json', 'utf8'))
+    var softwareProjects = json['software-projects']
+    var diyProjects = json['diy-projects']
+
+    res.render('index', {title: 'Alex Baratti', softwareProjects: softwareProjects, diyProjects: diyProjects});
 });
 
 module.exports = router;
