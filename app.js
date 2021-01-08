5b81e2584eea85988ef15c00c16084a2f3e20d7c
Alex Baratti <alexsmbaratti@gmail.com>
2021-01-07 20:08:00 -0800

    Sets amiibo display page to under construction

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var amiiboDisplayRouter = require('./routes/amiibo-display');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Controller
 app.use('/', indexRouter);
+app.use('/amiibo-display', amiiboDisplayRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
