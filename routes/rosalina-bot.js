0d1874a575253ddc450ece816026123ca87bae49
Alex Baratti <alexsmbaratti@gmail.com>
2021-01-17 17:05:54 -0700

    Adds navbar to RosalinaBot page

--- routes/rosalina-bot.js
+++ routes/rosalina-bot.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    var json = JSON.parse(fs.readFileSync('./data.json', 'utf8'))
+    var experience = json['experience']
+    var softwareProjects = json['software-projects']
+    var diyProjects = json['diy-projects']
+    var links = json['links']
+
     res.render('rosalina-bot', {
-        title: 'RosalinaBot'
+        title: 'RosalinaBot',
+        experience: experience,
+        softwareProjects: softwareProjects,
+        diyProjects: diyProjects,
+        links: links
     });
 });
 
