88f7c5ccb8b1765d345dfd2783b533f0e3ace449
Alex Baratti <alexsmbaratti@gmail.com>
2021-01-27 00:05:47 -0700

    Adds navbar to portfolio-site.ejs

--- routes/portfolio-site.js
+++ routes/portfolio-site.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    let json = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+    let experience = json['experience'];
+    let softwareProjects = json['software-projects'];
+    let diyProjects = json['diy-projects'];
+    let links = json['links'];
+
     res.render('portfolio-site', {
-        title: 'Portfolio Site'
+        title: 'Portfolio Site',
+        experience: experience,
+        softwareProjects: softwareProjects,
+        diyProjects: diyProjects,
+        links: links
     });
 });
 
